46ec0f79cfde97cf3e732e639e0a2c354ed3fd02
koshikawa <koshikawa@ppworks.jp>
2013-06-05 23:50:06 +0900

    Add User.authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ActiveRecord::Base
+  has_many :connections
+
+  class << self
+    def authentication(auth_hash, current_user = nil)
+      begin
+        params = {
+          provider: auth_hash['provider'],
+          uid: auth_hash['uid'],
+          name: auth_hash['info']['name'],
+          image: auth_hash['info']['image'],
+          access_token: auth_hash['credentials']['token'],
+          secret_token: auth_hash['credentials']['secret'],
+        }
+      rescue => e
+        return nil
+      end
+      
+      connection = Connection.where(provider: params[:provider], uid: params[:uid]).first_or_initialize
+      user = current_user ? current_user : connection.user
+      user = User.create(name: params[:name], image: params[:image]) unless user
+
+      params[:user] = user
+      connection.update_attributes(params)
+      
+      return user
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,73 @@ describe User do
     subject { user }
     it { should be_instance_of User }
   end
+
+  describe 'relations' do
+    it { should have_many(:connections) }
+  end
+
+  describe '.authentication' do
+    let(:auth_hash) { nil }
+    let(:current_user) { nil }
+    subject { User.authentication(auth_hash, current_user) }
+
+    context 'auth_hashがない' do
+      it { should be_nil }
+    end
+
+    context 'auth_hashがある' do
+      let(:auth_hash) {
+        {
+          'provider' => 'twitter',
+          'uid' => '12345',
+          'info' => {
+            'nickname' => 'ppworks',
+            'name' => 'PP works',
+            'image' => 'http://a0.twimg.com/profile_images/2900491556/9d2bf873770958647f18b8e61af31f1a_normal.png'
+          },
+          'credentials' => {
+            'token' => '123445678-AbeafjabutWjfav932m38e3TTabbbbbk',
+            'secret' => 'UzOc15tGx8AMYLOX5dcZ2UQTEwe6LiVysdoyhiKlaw'
+          }
+        }
+      }
+      
+      context 'current_userがいる(ログイン中の場合)' do
+        let(:current_user) { user }
+        context 'connectionがある' do
+          let(:connection) { FactoryGirl.build(:connection) }
+          before do
+            user.connections << connection
+          end
+          it { should eq user }
+          it { should be_instance_of User }
+        end
+
+        context 'connectionがない' do
+          it { should eq user }
+          it { should be_instance_of User }
+        end
+      end
+
+      context 'current_userがいない(未ログインの場合)' do
+        context 'connectionがある' do
+          let(:connection) { FactoryGirl.build(:connection) }
+          before do
+            user.connections << connection
+          end
+          it { should eq user }
+          it { should be_instance_of User }
+        end
+
+        context 'connectionがない' do
+          let(:new_user) { FactoryGirl.create(:user) }
+          before do
+            User.stub(:create).and_return(new_user)
+          end
+          it { should eq new_user }
+          it { should be_instance_of User }
+        end
+      end
+    end
+  end
 end
