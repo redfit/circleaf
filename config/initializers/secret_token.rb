cd76bfd3852dd09eec493257f6055cd4896ca3c1
koshikawa <koshikawa@ppworks.jp>
2013-06-05 19:23:33 +0900

    New rails application
    $ rails new ishikitakai.com --skip-bundle -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IshikitakaiCom::Application.config.secret_key_base = '8be64fd3b4e4ddc3ebe774fb993f1b2b36525be9b59727ff21cd8f6c6d31d545484666dde425ab0cb76a40de6c09241e1d362a200e4328f1f50e0bcb64d6be69'
