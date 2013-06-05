8383ebcc96c19ffb25480c03498a2bc9774ab8cc
koshikawa <koshikawa@ppworks.jp>
2013-06-06 08:07:30 +0900

    User can join/leave group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,19 @@ class Group < ActiveRecord::Base
   extend Enumerize
   LEVELS = [:public, :private].freeze
   enumerize :level, in: self::LEVELS
+
+  has_many :memberships
+  has_many :users, through: :memberships
+
+  def join(user, level = 'member')
+    user.memberships.create(group_id: self.id, level: level) unless user.groups.include?(self)
+  end
+
+  def leave(user)
+    user.groups.delete(self)
+  end
+
+  def membership_for(user)
+    user.memberships.where(group_id: self.id, user_id: user.id).first
+  end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,9 +3,26 @@ require 'spec_helper'
 
 describe Group do
   let(:group) { create(:group) }
+  let(:user) { create(:user) }
 
   describe 'グループを作成できる' do
     subject { group }
     it { should be_instance_of Group }
+  end
+
+  describe 'relations' do
+    it { should have_many(:users).through(:memberships) }
+  end
+
+  describe '#join' do
+    before do
+      group.join(user, level)
+    end
+    Membership::LEVELS.each do |l|
+      context "#{l.to_s}として加入する場合" do
+        let(:level) { l.to_s }
+        it { group.membership_for(user).level.should eq level }
+      end
+    end
   end
 end
