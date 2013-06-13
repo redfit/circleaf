250a308f7c70e5c0e53ba6d432e4a486384e7a5f
koshikawa <koshikawa@ppworks.jp>
2013-06-13 19:17:39 +0900

    Add Event#join & Event#leave methods

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,4 +6,23 @@ class Attendance < ActiveRecord::Base
 
   belongs_to :event
   belongs_to :user
+
+  before_save :check_capacity, if: -> (a) { a.status != 'cancel' }
+  before_save :update_pending_attendance, if: -> (a) { a.status == 'cancel' }
+
+  private
+  def check_capacity
+    if self.event.capacity_max <= self.event.attend_attendances.count
+      self.status = 'pending'
+    else
+      self.status = 'attend'
+    end
+  end
+
+  def update_pending_attendance
+    if 0 < self.event.pending_attendances.count
+      first_pending_attendance = self.event.pending_attendances.first
+      first_pending_attendance.update_column(:status, 'attend')
+    end
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,14 +2,14 @@
 
 FactoryGirl.define do
   factory :user, class: 'User' do
-    email Faker::Internet.email
-    name Faker::Name.name
-    image Faker::Internet.url
+    email { Faker::Internet.email }
+    name { Faker::Name.name }
+    image { Faker::Internet.url }
     default_provider_id 1
-    unconfirmed_email Faker::Internet.email
+    unconfirmed_email { Faker::Internet.email }
     confirm_limit_at { Date.current + 10.day }
     hash_to_confirm_email ''
     locale 'ja'
-    content Faker::Lorem.paragraph
+    content { Faker::Lorem.paragraph }
   end
 end
