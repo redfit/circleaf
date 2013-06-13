2fd0d488fcf6ac0b530248a43e3b52ba648196ff
koshikawa <koshikawa@ppworks.jp>
2013-06-14 07:43:36 +0900

    Implement event feature

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ActiveRecord::Base
+  extend Enumerize
+  extend ActiveModel::Naming
+  PRIVACY_SCOPES = [:public, :private].freeze
+  enumerize :privacy_scope, in: self::PRIVACY_SCOPES
+
   belongs_to :user
   belongs_to :group
   has_many :attendances, -> { order(updated_at: :asc) }
@@ -6,7 +11,9 @@ class Event < ActiveRecord::Base
   has_many :attend_attendances, -> { where(status: 'attend').order(updated_at: :asc, id: :asc) }, class_name: 'Attendance'
   has_many :pending_attendances, -> { where(status: 'pending').order(updated_at: :asc, id: :asc) }, class_name: 'Attendance'
 
-  validates_presence_of :user, :name
+  validates_presence_of :user, :name, :begin_at, :end_at
+
+  after_initialize :format_date
 
   def join(user)
     attendance = user.attendances.find_or_initialize_by(event_id: self.id)
@@ -22,5 +29,12 @@ class Event < ActiveRecord::Base
 
   def attendance_for(user)
     user.attendances.where(event_id: self.id).first
+  end
+
+  private
+  def format_date
+    [:begin_at, :end_at].each do |d|
+      self[d] = self[d].try(:strftime, '%Y-%m-%d %H:%M')
+    end
   end
 end
