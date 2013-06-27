1c3b7963c021b26869d3495701a44f10591c1831
koshikawa <koshikawa@ppworks.jp>
2013-06-28 03:18:06 +0900

    User colud change own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,26 @@ class UsersController < ApplicationController
     @user = User.find(params[:id] || current_user.try(:id))
   end
 
+  def edit
+  end
+
+  def update
+    @user.attributes = user_param
+    if @user.save
+      redirect_to edit_my_path, notice: t('users.edit.updated')
+    else
+      render :edit
+    end
+  end
+
   private
   def set_user
     @user = User.find(current_user.id)
+  end
+
+  def user_param
+    params.require(:user).permit(
+      :content,
+    )
   end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -35,4 +35,39 @@ describe "Users" do
       end
     end
   end
+
+  describe 'プロフィール編集' do
+    context 'ログインしていない' do
+      before do
+        visit edit_my_path
+      end
+      it 'ログイン画面へ遷移する' do
+        current_path.should eq root_path
+      end
+    end
+
+    context 'ログインしている' do
+      before do
+        oauth_sign_in(user, :twitter)
+        visit edit_my_path
+      end
+      it '画面遷移する' do
+        current_path.should eq edit_my_path
+      end
+
+      describe '更新する' do
+        let(:new_content) { Faker::Lorem.paragraph }
+        before do
+          find('#wmd-input').set(new_content)
+          find('form input[type=submit]').click()
+        end
+        it 'メッセージが表示される' do
+          page.should have_content(I18n.t('users.edit.updated'))
+        end
+        it '更新されたプロフィールが表示される' do
+          page.should have_content(new_content)
+        end
+      end
+    end
+  end
 end
